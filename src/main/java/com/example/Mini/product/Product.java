cfd66677703fd87b38a22d2188b1a4f70a3b6110
LyHongDang <lyhongdang03@gmail.com>
2025-08-14 20:37:46 +0700

    Crud product

---
+++ src/main/java/com/example/Mini/product/Product.java
@@ -0,0 +1,25 @@
+package com.example.Mini.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String productName;
+    private Long price;
+    private Long quantity;
+    private String imageUrl;
+}
---
+++ src/main/java/com/example/Mini/user/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.Mini.user;
+
+public class UserRepository {
+
+}
